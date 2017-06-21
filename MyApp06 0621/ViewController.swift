67699f03fd3f1b967c7c9375a612e4080fca63e4
iii-user <iii-user@iii-userde-MacBook-4.local>
2017-06-21 11:27:32 +0800

    u

--- MyApp06 0621/ViewController.swift
+++ MyApp06 0621/ViewController.swift
@@ -10,8 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
+    @IBOutlet weak var svalue: UISwitch!
+    
+    @IBOutlet weak var stepValue: UIStepper!
+    
+    @IBOutlet weak var mybtn : UIButton!
+    
+    private var isOn:Bool?
+    
+    @IBAction func switchStatus(_ sender: Any) {
+        print(svalue.isOn)
+    }
+    @IBAction func changeVlaue(_ sender: Any) {
+        print(stepValue.value)
+    }
+    
+    @IBAction func clickButton(_ sender: Any) {
+        mybtn.isSelected = !mybtn.isSelected
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        stepValue.maximumValue = 10
+        stepValue.minimumValue = 0
+        stepValue.value = 5
+        mybtn.setImage(UIImage(named:"switchon"), for :UIControlState.normal)
+        mybtn.setImage(UIImage(named:"switch off"), for :UIControlState.selected)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
